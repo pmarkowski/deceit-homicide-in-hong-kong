7cf3acb9ce5f575d2c2158d1cb22f78aa1162614
Philip Markowski <philipmarkowski642@gmail.com>
2021-11-19 12:30:58 -0500

    Nest StartGameData class into Action class

--- server/Deceit.Domain/Game/States/Actions/StartGameAction.cs
+++ server/Deceit.Domain/Game/States/Actions/StartGameAction.cs
@@ -2,16 +2,15 @@
 
 namespace Deceit.Domain.Game.States.Actions;
 
-public class StartGameData
+public class StartGameAction : ActionBase<StartGameAction.StartGameData>
 {
-    public IEnumerable<Player> Players { get; init; }
-    public string ForensicScientistPlayerId { get; init; }
-    public int NumberOfEvidenceCards => 4;
+    public class StartGameData
+    {
+        public IEnumerable<Player> Players { get; init; }
+        public string ForensicScientistPlayerId { get; init; }
+        public int NumberOfEvidenceCards => 4;
+    }
 
-}
-
-public class StartGameAction : ActionBase<StartGameData>
-{
     public StartGameAction(StartGameData data) : base(data)
     {
     }
