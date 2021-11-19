b2b2ec2a779d4e5df31b4b4fb83c67b1cf701dee
Philip Markowski <philipmarkowski642@gmail.com>
2021-11-19 12:42:04 -0500

    Add a note for later

--- server/tests/Deceit.Backend.Tests/Game/States/CrimeStateTests.cs
+++ server/tests/Deceit.Backend.Tests/Game/States/CrimeStateTests.cs
@@ -78,6 +78,9 @@ public class CrimeStateTests
             ForensicScientistPlayerId = forensicScientist.ConnectionId
         }));
 
+        // TODO: Most of the public interface obscures this information, but you can
+        // still figure out the murderer from the public interface through this.
+        // It's handy for this test, but will want to revisit this later.
         var murderer = context.Game.Investigators!.Single(investigator => investigator.Role == Roles.Murderer);
 
         KeyEvidence selectedKeyEvidence = new(
