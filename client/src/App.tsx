14e6f9831b199c7abf14e6b118e95c987b8be3ee
Philip Markowski <philipmarkowski642@gmail.com>
2021-11-21 21:34:29 -0500

    Add basic landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+
+  const clickJoinLobby = () => { };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <div className="min-h-screen text-center bg-trueGray-900">
+      <div className="py-16">
+        <h1 className="text-9xl text-red-700 font-medium uppercase tracking-wider font-serif">Deceit</h1>
+        <h2 className="text-4xl text-red-700 font-extralight uppercase tracking-widest">Homicide in Hong Kong</h2>
+        <p className="pt-6 text-lightBlue-200">
+          An online implementation of <a href="https://greyfoxgames.com/deception-murder-in-hong-kong/" target="_blank" rel="noreferrer">Deception: Murder in Hong Kong</a>.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+      <div className="py-16 space-y-8">
+        <div>
+          <a className="btn bg-lightBlue-500 text-black hover:bg-lightBlue-400" href="/lobby">Create Lobby</a>
+        </div>
+        <div>
+          <input className="block bg-lightBlue-100 text-black font-semibold rounded-md p-2 mx-auto text-center placeholder-trueGray-600" placeholder="Lobby Code" />
+          <button className="btn bg-lightBlue-500 text-black hover:bg-lightBlue-400" onClick={clickJoinLobby}>Join Lobby</button>
+        </div>
+      </div>
+    </div >
   );
 }
 
