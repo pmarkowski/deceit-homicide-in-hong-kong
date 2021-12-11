b4739b3c9365a15234cc36d9b2b48652eaea0207
Philip Markowski <philipmarkowski642@gmail.com>
2021-12-11 14:57:54 -0500

    Connect to signalr hub in Lobby component

--- client/src/Lobby.tsx
+++ client/src/Lobby.tsx
@@ -1,7 +1,33 @@
+import { useState } from "react";
 import { useParams } from "react-router";
+import * as signalR from "@microsoft/signalr";
+
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl(`${process.env.REACT_APP_SERVER_BASE_URL}/pregame`)
+    .build();
 
 export const Lobby = () => {
     const params = useParams();
 
-    return <span>Loading up {params.lobbyId}</span>
+    const [username, setUsername] = useState("");
+
+    const joinLobby = () => {
+        console.log(username);
+        connection.start()
+            .then(() => {
+                connection.invoke("ConnectPlayer", params.lobbyId, username);
+            });
+    };
+
+    return <div>
+        <h1>Connecting to {params.lobbyId}</h1>
+        <label>
+            Username:
+            <input
+                className="border border-black"
+                value={username}
+                onChange={event => setUsername(event.currentTarget.value)} />
+        </label>
+        <button className="block btn bg-gray-300" onClick={joinLobby}>Join lobby</button>
+    </div>;
 };
