56dae9f17b48cc3ef11336a6a055f773751c7f85
Philip Markowski <philipmarkowski642@gmail.com>
2022-01-09 19:47:34 -0500

    Add key to player list

--- client/src/LobbyPlayerList.tsx
+++ client/src/LobbyPlayerList.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 export const LobbyPlayerList: FC<{ connectedPlayers: any[], forensicScientistId: string }> = (props) =>
     <>
         {props.connectedPlayers.map(player =>
-            <p className="text-xl">
+            <p className="text-xl" key={player.connectionId}>
                 {`${player.name}${player.connectionId === props.forensicScientistId ? " | Forensic Scientist" : ""}`}
             </p>)}
     </>
